02772fc743e7eabbb2b2b184a484df68301bca14
MassiPozzuto <massipozzuto2016@gmail.com>
2024-05-17 21:35:09 -0300

    avanzo con opciones global y singular

--- Chat C-S Threading con Auth/server.py
+++ Chat C-S Threading con Auth/server.py
@@ -5,6 +5,16 @@ from database import Database
 # Lista para almacenar todos los clientes conectados
 connected_clients = []
 
+def commands(command, client_socket, database, user):
+    if command == 'help':
+        client_socket.send("Los comandos disponibles son: \n'/global': Enviar mensajes por el chat global\n'/sendTo <username>': Enviar mensajes a un usuario en especifico\n'/info': Indica el chat en el que te encuentras en ese momento")
+    elif command == 'global':
+        return 0
+    elif command == 'sendTo':
+        return 0
+    elif command == 'info':
+        return 0
+
 # Función para reenviar los mensajes de un cliente a todos los demas clientes
 def broadcast(message, sender_socket, user):
     for client_socket, _ in connected_clients:
@@ -22,11 +32,11 @@ def handle_client(client_socket, client_address, database):
     print(f"Conexión aceptada de {client_address[0]}:{client_address[1]}")
     
     #Log-in
-    client_socket.send("Servidor: Para conectarte al chat, primero debes iniciar sesión.".encode())
+    client_socket.send("Para conectarte al chat, primero debes iniciar sesión.".encode())
     while True:
         client_socket.send("Ingresa el nombre de usuario: ".encode())
         username = client_socket.recv(1024).decode()
-        client_socket.send("Ingresa la contraseña".encode())
+        client_socket.send("Ingresa la contraseña:/".encode())
         password = client_socket.recv(1024).decode()
 
         user = database.DBQuery(f"SELECT * FROM usuarios WHERE username = '{username}' AND password = '{password}'")
@@ -42,17 +52,20 @@ def handle_client(client_socket, client_address, database):
     connected_clients.append((client_socket, client_address))
     
     # Recibo los mensajes del cliente y los reenvió a los demás
-    client_socket.send(f"Escribe en la consola para enviar un mensaje por el chat global :)".encode())
+    client_socket.send(f"Te encuentras en el chat global, tus mensajes los podrán ver todos. Para saber como enviar mensajes a alguien especifico ingresa '/help'".encode())
     while True:
         message = client_socket.recv(1024).decode()
         if not message:
             break
         print(f"Mensaje recibido de {client_address[0]}:{client_address[1]}: {message}")
     
-        # Guardo el mensaje en la base de datos
-        database.DBQuery(f"INSERT INTO mensajes(id_origen, mensaje, id_destino) VALUES ({user['id']},'{message}', NULL)")
-        # Llamo a la funcion broadcast para reenviar este mensaje a los demas clientes
-        broadcast(message, client_socket, username)
+        if message[0] == '/':
+            commands()
+        else:
+            # Guardo el mensaje en la base de datos
+            database.DBQuery(f"INSERT INTO mensajes(id_origen, mensaje, id_destino) VALUES ({user['id']},'{message}', NULL)")
+            # Llamo a la funcion broadcast para reenviar este mensaje a los demas clientes
+            broadcast(message, client_socket, username)
 
     print(f"Conexión cerrada de {client_address[0]}:{client_address[1]}")
     client_socket.close()
