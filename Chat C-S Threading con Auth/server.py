8f428d8dddbe901d8b67d6dd2f362a2b8af8fdc2
MassiPozzuto <massipozzuto2016@gmail.com>
2024-05-17 21:04:47 -0300

    Agrego funcionalidad de login y guardo los mensajes en la BD, falta opcion de enviar mensajes a una sola persona

---
+++ Chat C-S Threading con Auth/server.py
@@ -0,0 +1,82 @@
+import socket
+import threading
+from database import Database
+
+# Lista para almacenar todos los clientes conectados
+connected_clients = []
+
+# Función para reenviar los mensajes de un cliente a todos los demas clientes
+def broadcast(message, sender_socket, user):
+    for client_socket, _ in connected_clients:
+        if client_socket != sender_socket:
+            try:
+                client_socket.send(f"{user['username']}: {message}".encode())
+            except:
+                # En caso de que haya un error al enviar el mensaje a algun cliente, se cierra la conexión y se elimina de la lista de clientes conectados
+                client_socket.close()
+                connected_clients.remove((client_socket, _))
+
+
+# Función que contiene lo que realizan los threads
+def handle_client(client_socket, client_address, database):
+    print(f"Conexión aceptada de {client_address[0]}:{client_address[1]}")
+    
+    #Log-in
+    client_socket.send("Servidor: Para conectarte al chat, primero debes iniciar sesión.".encode())
+    while True:
+        client_socket.send("Ingresa el nombre de usuario: ".encode())
+        username = client_socket.recv(1024).decode()
+        client_socket.send("Ingresa la contraseña".encode())
+        password = client_socket.recv(1024).decode()
+
+        user = database.DBQuery(f"SELECT * FROM usuarios WHERE username = '{username}' AND password = '{password}'")
+                
+        if(len(user) == 1):
+            user = user[0]
+            client_socket.send("Se ha iniciado sesión correctamente".encode())
+            break
+        else:
+            client_socket.send("Error al iniciar sesión. Nombre de usuario y/o contraseña incorrectos".encode())
+        
+    # Se agrega el nuevo cliente a la lista de clientes conectados
+    connected_clients.append((client_socket, client_address))
+
+    # Recibo los mensajes del cliente y los reenvió a los demás
+    client_socket.send(f"Escribe en la consola para enviar un mensaje por el chat global :).".encode())
+    while True:
+        message = client_socket.recv(1024).decode()
+        if not message:
+            break
+        print(f"Mensaje recibido de {client_address[0]}:{client_address[1]}: {message}")
+        
+        # Guardo el mensaje en la base de datos
+        database.DBQuery(f"INSERT INTO mensajes(id_origen, mensaje, id_destino) VALUES ({user['id']},'{message}', NULL)")
+        
+        # Llamo a la funcion broadcast para reenviar este mensaje a los demas clientes
+        broadcast(message, client_socket, username)
+
+    print(f"Conexión cerrada de {client_address[0]}:{client_address[1]}")
+    client_socket.close()
+    connected_clients.remove((client_socket, client_address, user))
+    
+
+# Función principal se abre el servidor y se crean los threads
+def main():
+    HOST = '127.0.0.1'
+    PORT = 65432
+
+    # Inicializo la conexion con la base de datos 'chat'
+    database = Database("chat")
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+        server_socket.bind((HOST, PORT))
+        server_socket.listen(5)
+        print(f"Servidor escuchando en {HOST}:{PORT}")
+
+        while True:
+            client_socket, client_address = server_socket.accept()
+            client_thread = threading.Thread(target=handle_client, args=(client_socket, client_address, database))
+            client_thread.start()
+
+if __name__ == "__main__":
+    main()
---
+++ chat.sql
@@ -0,0 +1,88 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 20-04-2024 a las 00:04:22
+-- Versión del servidor: 10.4.27-MariaDB
+-- Versión de PHP: 8.0.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `chat`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `mensajes`
+--
+
+CREATE TABLE `mensajes` (
+  `id` int(11) NOT NULL,
+  `id_origen` int(11) NOT NULL,
+  `mensaje` varchar(100) NOT NULL,
+  `id_destino` int(11) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `email` varchar(40) NOT NULL,
+  `power` varchar(15) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `deleted_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `mensajes`
+--
+ALTER TABLE `mensajes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `mensajes`
+--
+ALTER TABLE `mensajes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
